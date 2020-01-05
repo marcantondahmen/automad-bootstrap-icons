c4aaff9ccf84031e6984f75826cc84b53f0a7263
Marc Anton Dahmen <mail@marcdahmen.de>
2020-01-05 17:28:21 +0100

    Initial commit

---
+++ Icon.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Automad\Bootstrap;
+
+defined('AUTOMAD') or die('Direct access not permitted!');
+
+class Icon {
+
+	
+	/**
+	 *	The main function to embed an icon within a template.
+	 *
+	 * 	@param array $options
+	 * 	@param object $Automad
+	 * 	@return string The SVG markup  
+	 */
+
+	public function Icon($options, $Automad) {
+
+		$defaults = array(
+			'icon' => 'alert-circle',
+			'w' => '1.5em',
+			'h' => '1.5em',
+			'viewBox' => '0 0 20 20'
+		);
+
+		$options = array_merge($defaults, $options);
+
+		$file = __DIR__ . '/bootstrap-icons/icons/' . $options['icon'] . '.svg';
+
+		$svg = file_get_contents($file);
+		return str_replace(
+			array(
+				'width="1em"',
+				'height="1em"', 
+				'viewBox="0 0 20 20'
+			), 
+			array(
+				'width="' . $options['w'] . '"', 
+				'height="' . $options['h'] . '"', 
+				'viewBox="' . $options['viewBox'] . '"'
+			),
+			$svg
+		);
+		
+	}
+
+}
